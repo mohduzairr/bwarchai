d5ec5c84b7c8d9843df6654576b1caab215fd25e
uzairr <mohduzair1990@gmail.com>
2021-12-15 17:05:25 +0530

    features text alignment

--- src/screen/HomeScreen/index.jsx
+++ src/screen/HomeScreen/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import { ImageBackground, Text,View,Image,StyleSheet, ScrollView,TouchableOpacity} from 'react-native'
- 
+import { ImageBackground, Text,View,Image,StyleSheet, ScrollView,TouchableOpacity} from 'react-native';
+import {  useFonts, Raleway_200ExtraLight} from '@expo-google-fonts/raleway';
+import AppLoading from 'expo-app-loading';
+// import {  useFonts, Nunito_200ExtraLight_Italic,} from '@expo-google-fonts/nunito';
+
+
+
  export const HomeScreen = () => {
    
     const image ={ uri: "https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1633589090002x146338665670503680%2Fbackground50001.jpg?w=1536&amp;h=&amp;auto=compress&amp;dpr=1&amp;fit=max" };
@@ -13,7 +18,7 @@ import { ImageBackground, Text,View,Image,StyleSheet, ScrollView,TouchableOpacit
         <View style={styles.container}>
 
             <Image source={{uri: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1633590093985x133676610762875950%2Fbreelaun_Metal%2520%25283%2529.png?w=384&amp;h=288&amp;auto=compress&amp;fit=crop&amp;dpr=1'}}
-       style={{width: 350, height: 120,marginLeft:20,marginTop:80,borderWidth:1}} /> 
+       style={{width: 350, height: 140,marginLeft:20,marginTop:90,borderWidth:1}} /> 
             
          <Image source={{uri: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1633590332624x150773598429397860%2Fbreelaun_Metal%2520%25282%2529%2520%25281%2529.png?w=768&amp;h=535&amp;auto=compress&amp;fit=crop&amp;dpr=1'}}
        style={{width: 400, height: 300}} /> 
@@ -32,12 +37,12 @@ import { ImageBackground, Text,View,Image,StyleSheet, ScrollView,TouchableOpacit
           {/* <Features/> */}
          <View style= {{width:'100%',height:700,backgroundColor:'rgba(128, 128, 128, 0.8)',marginTop:60}}>
            <Text style={{textAlign:'center',fontSize:40,marginTop:20,fontWeight: 'bold',borderBottomWidth: 5,width:300,marginLeft:40,borderBottomColor: "rgba(255, 223, 0, 0.6)"}}>FEATURES</Text>
-           < Text style={{marginTop:20,paddingLeft:10,lineHeight:35,fontSize:20,paddingTop:10,textAlign: 'justify'}} >
+           < Text style={{marginTop:20,lineHeight:35,fontSize:20,paddingTop:10,textAlign: 'center', fontFamily:'Raleway_200ExtraLight'}} >
            Bmarchai Studios gives you the chance to be have free access to the same fitness secrets taught to some real models in L.A., and many of the workouts  have things you've never seen before. Way more that just squats and push-ups, are {'\n'} workouts include; 
            {'\n'}  lifting the breast area, inner thigh gap, toning the bra strap area, 
              lower pouch area (no not just knee tucks).{'\n'} and more. 
            </Text>
-           <Text style={{paddingLeft:10,lineHeight:35,fontSize:20,paddingTop:10,textAlign: 'justify'}} > There's a free workout everyday, with no extra subscription needed,we don't even have a premium area because it's all premium</Text>
+           <Text style={{lineHeight:35,fontSize:20,paddingTop:10,textAlign: 'center'}} > There's a free workout everyday, with no extra subscription needed,{'\n'}we don't even have a premium area because it's all premium.</Text>
          </View>
           {/* <WorkOut/> */}
         <View>
@@ -72,7 +77,11 @@ import { ImageBackground, Text,View,Image,StyleSheet, ScrollView,TouchableOpacit
         </View>
 
        </View>  
-      
+       
+       <View style={{height:100,width:'100%'}}>
+
+       </View>
+
         </ImageBackground>
         </ScrollView>
 
